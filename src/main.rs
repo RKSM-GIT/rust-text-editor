c064013468a7cb243a62d14cb7ef141152d2f8c9
RKSM-GIT <m.r.k.s@iitg.ac.in>
2025-01-16 16:07:21 +0530

    Splitting code into multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-
-        if c.is_control() {
-            println!("BYTES: {0:08b}, ASCII: {0:#03}\r", b);
-        } else {
-            println!("BYTES: {0:08b}, ASCII: {0:#03}, CHAR: {1:#?}\r", b, c);
-        }
-
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
